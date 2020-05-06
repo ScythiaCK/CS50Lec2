c544ac1451197fbcaafe5f9f197155016aa33bc4
ScythiaCK <eliotplayzbnr@gmail.com>
2020-05-06 10:37:53 +0800

    Added names.py

---
+++ names.py
@@ -0,0 +1,4 @@
+import random
+names = ("Ashley", "Bonnie", "Charlie", "David", "Esther", "Fiona", "George", "Harper", "Isabelle", "Jessica", "Kyle", "Leonard", "Mandy", "Neil", "Olivia", "Penelope", "Queenie", "Richard", "Shawn", "Tasha", "Ulysses", "Vanessa", "Wyatt", "Xavier", "Yvonne", "Zelda")
+for name in names:
+  print(name)
