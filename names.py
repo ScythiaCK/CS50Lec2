23ce901b9ea691e8cffea7f6eb78cae34574bf2f
ScythiaCK <eliotplayzbnr@gmail.com>
2020-05-06 11:07:31 +0800

    Finally finished the py

--- names.py
+++ names.py
@@ -1,4 +1,25 @@
 import random
 names = ("Ashley", "Bonnie", "Charlie", "David", "Esther", "Fiona", "George", "Harper", "Isabelle", "Jessica", "Kyle", "Leonard", "Mandy", "Natasha", "Olivia", "Penelope", "Queenie", "Richard", "Shawn", "Tasha", "Ulysses", "Vanessa", "Wyatt", "Xavier", "Yvonne", "Zelda")
+scores = {}
 for name in names:
-  print(name)
+    while True:
+        try:
+            scores[name] = int(input("What is the score for "+str(name)+"?"))
+            break
+        except:
+            print("You should choose an integer.")
+print(scores)
+
+for i in range(50):
+    print("\n")
+
+
+while True:
+
+    guess = input("Guess a name.")
+    if guess in names:
+        score = str(scores[guess])
+        print("Your score is "+ score)
+        break
+    else:
+        print("Your guess is wrong. Please try again.")
