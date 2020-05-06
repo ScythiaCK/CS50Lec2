3ce9fc518f5e02f59bc0484f0b6de8e2db548ef9
ScythiaCK <eliotplayzbnr@gmail.com>
2020-05-06 12:12:46 +0800

    hi
    hi

--- classes.py
+++ classes.py
@@ -2,8 +2,13 @@ class CartCoord:
     def __init__(self, x, y):
         self.y = y
         self.x = x
+        self.coord = "(" + str(x) + ", " + str(y) + ")"
 
 class PolCoord:
     def __init__(self, r, theta):
         self.r = r
         self.theta = theta
+
+coordinates = CartCoord(3,5)
+print(coordinates.coord)
+print(coordinates.x)
