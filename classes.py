69ff8c45551c46243238193b808c517955891c55
ScythiaCK <eliotplayzbnr@gmail.com>
2020-05-06 12:06:47 +0800

    Made 2 classes in classes.py (Cart and Pol Coord)

---
+++ classes.py
@@ -0,0 +1,9 @@
+class CartCoord:
+    def __init__(self, x, y):
+        self.y = y
+        self.x = x
+
+class PolCoord:
+    def __init__(self, r, theta):
+        self.r = r
+        self.theta = theta
